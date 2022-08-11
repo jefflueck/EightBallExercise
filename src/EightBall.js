113afcc36b31a0ecc2f2ecece081bedd58ce33e0
jefflueck <lueckcfd@yahoo.com>
2022-08-11 16:25:11 -0500

    Finished main part, Furher Study 1 of exercise

---
+++ src/EightBall.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './EightBall.css';
+import answers from './answers';
+
+const EightBall = () => {
+  const [answer, setAnswer] = useState('Think of a Question');
+  const [color, setColor] = useState('black');
+  const [guess, setGuess] = useState(0);
+  const newAnswer = () => {
+    const { msg, color } = answers[Math.floor(Math.random() * answers.length)];
+    setAnswer(msg);
+    setColor(color);
+    setGuess(guess + 1);
+  };
+  const reset = () => {
+    setAnswer('Think of a Question');
+    setColor('black');
+    setGuess(0);
+  };
+  return (
+    <>
+      <h1 className="EightBall-header">The Magic Eight Ball!</h1>
+      <p className="EightBall-guesses">
+        It checked the wisdom of the EightBall{' '}
+        <span className="EightBall-guess-number">{guess}</span> times!
+      </p>
+      <div className="EightBall" style={{ backgroundColor: color }}>
+        <b className="EightBall-text">{answer}</b>
+      </div>
+      <div className="EightBall-button-group">
+        <button className="EightBall-button-guess" onClick={newAnswer}>
+          Ask Again!
+        </button>
+        <button className="EightBall-button-reset" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default EightBall;
---
+++ src/Render.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './EightBall.css';
+
+const Render = () => {
+  return <h1>Render</h1>;
+};
+
+export default Render;
